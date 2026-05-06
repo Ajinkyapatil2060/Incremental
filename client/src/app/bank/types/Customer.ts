627568d419db588f3294b004d48e2a5e9620ad9f
Ajinkyapatil2060 <dummy@gmail.com>
2026-05-06 19:20:28 +0000

    Day 25 Completed/ Showing Error in log in html

--- client/src/app/bank/types/Customer.ts
+++ client/src/app/bank/types/Customer.ts
@@ -1,12 +1,12 @@
 export class Customer {
-  customerId?: string;
-  name: string;
-  email: string;
-  password:string;
-  username:string;
-  role?:string;
+    customerId?: string;
+    name: string;
+    email: string;
+    password: string;
+    username: string;
+    role?: string;
 
-    constructor(data:any) {
+    constructor(data: any) {
         this.customerId = data.customerId;
         this.name = data.name;
         this.email = data.email;
@@ -14,17 +14,8 @@ export class Customer {
         this.password = data.password;
         this.role = data.role;
     }
-
-    displayInfo() {
-        console.log(`Customer ID: ${this.customerId}`);
-        console.log(`Name: ${this.name}`);
-        console.log(`Email: ${this.email}`);
-        console.log(`Username: ${this.username}`);
-        console.log(`Password: ${this.password}`);
-        console.log(`Role: ${this.role}`);
-        console.log("------");
-    }
 }
+
 
 
 
