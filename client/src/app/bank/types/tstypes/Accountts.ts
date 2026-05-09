b34333d50e135725fa666308e58140f2b3f0d47b
Ajinkyapatil2060 <dummy@gmail.com>
2026-05-09 18:50:23 +0000

    project done css remaining

--- client/src/app/bank/types/tstypes/Accountts.ts
+++ client/src/app/bank/types/tstypes/Accountts.ts
@@ -1,18 +1,20 @@
 export class AccountTS {
-    accountid?:string;
-    customerid:string;
-    balance:number;
-
-    constructor(customerid:string, balance:number, accountid?:string){
-        this.customerid = customerid;
+    accountId?: string;
+    customerId: string;
+    balance: number;
+    constructor( customerId: string, balance: number,accountId?:string,) {
+        this.accountId = accountId;
+        this.customerId = customerId;
         this.balance = balance;
-        this.accountid = accountid;
     }
 
-    displayInfo():void{
-        console.log(`Account ID: ${this.accountid}`);
-        console.log(`Customer ID: ${this.customerid}`);
+    displayInfo() {
+        console.log(`Account ID: ${this.accountId}`);
+        console.log(`Customer ID: ${this.customerId}`);
         console.log(`Balance: ${this.balance.toFixed(2)}`);
     }
-   
 }
+
+const account = new AccountTS("1",1000.00,"1");
+account.displayInfo();
+
